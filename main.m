681ef0acf2d2a154f95d71aa9a0ea5d1cc2eb978
sho <sho@DESKTOP-1D4KHED>
2021-02-21 01:34:44 -0500

    Addition of geometry tab and graphs

--- main.m
+++ main.m
@@ -3,10 +3,17 @@ addpath('func/');
 addpath('func/geo/');
 addpath('func/mesh/');
 addpath('func/classes/');
-% addpath('src/');
+addpath('src/');
 addpath('ui/');
 
 % uipt;
 
+% % g = holeplate(40,200,100,20,10);
+% % g = dholeplate(40,200,100,20,100,20,10);
+% % g = tplate(40,200,100,20,20);
+% g = rhplate(40,200,15);
+% pdegplot(g, 'EdgeLabels', 'on', 'VertexLabels', 'on', ...
+%     'FaceLabels', 'on');
+% axis equal;
 
 
