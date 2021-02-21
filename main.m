484f60d4e2177da5874021473ee95918a0ff6a89
sho <sho@DESKTOP-1D4KHED>
2021-02-21 09:54:46 -0500

    Complete fixing of UI

--- main.m
+++ main.m
@@ -6,14 +6,5 @@ addpath('func/classes/');
 addpath('src/');
 addpath('ui/');
 
-% uipt;
-
-% % g = holeplate(40,200,100,20,10);
-% % g = dholeplate(40,200,100,20,100,20,10);
-% % g = tplate(40,200,100,20,20);
-% g = rhplate(40,200,15);
-% pdegplot(g, 'EdgeLabels', 'on', 'VertexLabels', 'on', ...
-%     'FaceLabels', 'on');
-% axis equal;
-
+uipt;
 
